2e4eb8159944bb5e79ef119619500791a1141b14
minho <longfei6671@163.com>
2016-06-03 21:11:59 +0800

    添加字体

--- example/main.go
+++ example/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"log"
 	"html/template"
+	"os"
+	"strings"
+	"io/ioutil"
 )
 const (
 	dx = 200
@@ -14,15 +17,29 @@ const (
 
 func main() {
 
-	gocaptcha.SetFontFamily("fonts/BRACELET.ttf","fonts/ApothecaryFont.ttf");
-	gocaptcha.SetFontFamily("fonts/actionj.ttf");
-	gocaptcha.SetFontFamily("fonts/Comismsh.ttf");
-	gocaptcha.SetFontFamily("fonts/Esquisito.ttf");
-	gocaptcha.SetFontFamily("fonts/DENNEthree-dee.ttf");
+	//fontFils,err := ListDir("fonts",".ttf");
+	//if(err != nil){
+	//	fmt.Println(err);
+	//	return ;
+	//}
+	//
+	//gocaptcha.SetFontFamily(fontFils...);
+
+	gocaptcha.SetFontFamily(
+		"fonts/3Dumb.ttf",
+		"fonts/DeborahFancyDress.ttf",
+		"fonts/actionj.ttf",
+		"fonts/chromohv.ttf",
+		"fonts/D3Parallelism.ttf",
+		"fonts/Flim-Flam.ttf",
+		"fonts/KREMLINGEORGIANI3D.ttf",
+		//"fonts/Lead.ttf",
+		)
 
 
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/get/", Get)
+	fmt.Println("服务已启动...");
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal(err)
@@ -51,4 +68,24 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	}
 
 	captchaImage.SaveImage(w,gocaptcha.ImageFormatJpeg);
+}
+
+//获取指定目录下的所有文件，不进入下一级目录搜索，可以匹配后缀过滤。
+func ListDir(dirPth string, suffix string) (files []string, err error) {
+	files = make([]string, 0, 10)
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+	PthSep := string(os.PathSeparator)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			continue
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
+			files = append(files, dirPth+PthSep+fi.Name())
+		}
+	}
+	return files, nil
 }
